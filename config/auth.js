d3c2b028e4f4064e13da0d48cb5cd55f996861b7
EoinTraynor <eointraynor1@gmail.com>
2016-04-07 23:00:13 +0000

    Creation of Facebook Developer App

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientId': '1693988734209045',
+		'clientSecret': 'de0c38ff19e1f89d9fd8211569e0d3b8',
+		'callbackURL': 'http://192.168.33.10:3000/auth/facebook/callback'
+	}
+};
