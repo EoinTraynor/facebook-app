5ec36b7c171b658ae76862b3875d8a11486e53c3
EoinTraynor <eointraynor1@gmail.com>
2016-04-11 13:09:46 +0000

    UI update, Draw to canvas update

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,68 +1,74 @@
+// runs when document has loaded
 function init() {
 	console.log("Ready");
-
+	// Canvas Element
 	var canvas = document.getElementById('canvas');
 	var ctx = canvas.getContext("2d");
+
+	// Get profile image source
 	var profileSrc = new Image();
 	profileSrc.src = document.getElementById('profile').getAttribute('src');
+	profileSrc.setAttribute('crossOrigin', 'anonymous');
 
+	// draw image to canvas
 	ctx.drawImage(profileSrc, 0, 0);
 
+	// Eventlistener for option change
 	var opt = document.getElementById('option');
 	opt.addEventListener('change', function() {
 		var optionSelected = opt.options[opt.selectedIndex].text;
 		// Add layer to canvas
-		draw(ctx, optionSelected);
+		draw(ctx, optionSelected, profileSrc);
 	}, false);
 
 	// Download Image
 	document.getElementById('download').addEventListener('click', function() {
-		downloadCanvas(this, 'canvas', 'test.png');
+		downloadCanvas(this, canvas, 'test.png');
 	}, false);
-
-}
-
-function readSelect() {
 
 }
 
 function downloadCanvas(link, canvasId, filename) {
 	console.log(link, canvasId, filename);
-	window.open(canvas.toDataURL("image/png"));
+	window.open(canvasId.toDataURL("image/png"));
     // link.href = document.getElementById(canvasId).toDataURL();
     // link.download = filename;
 }
 
-function draw(ctx, option) {
+function draw(ctx, option, profileImage) {
 	var color;
 
+	// new option = redraw the profile picture on canvas
+	ctx.drawImage(profileImage, 0, 0);
+
 	// Check selection
+	// change color depending on option
 	if (option == "Fine Gale") {
-		color = 'rgba(255, 0, 0, 0.5)';
+		color = 'rgba(68, 80, 183, 0.5)';
 	}
 	else if (option == "Fianna Fail") {
-		color = 'rgba(255, 0, 0, 0.5)';
+		color = 'rgba(64, 179, 79, 0.5)';
 	}
 	else if (option == "Sinn Fein") {
-		color = 'rgba(255, 0, 0, 0.5)';
+		color = 'rgba(4, 96, 89, 0.5)';
 	}
 	else if (option == "Independant") {
-		color = 'rgba(255, 0, 0, 0.5)';
+		color = 'rgba(248, 176, 24, 0.5)';
 	}
 	else if (option == "Labour Party") {
-		color = 'rgba(255, 0, 0, 0.5)';
+		color = 'rgba(238, 58, 70, 0.5)';
 	}
 	else if (option == "AAA-PBP") {
-		color = 'rgba(255, 0, 0, 0.5)';
+		color = 'rgba(241, 90, 36, 0.5)';
 	}
 	else if (option == "Social Democrats") {
-		color = 'rgba(255, 0, 0, 0.5)';
+		color = 'rgba(118, 47, 138, 0.5)';
 	}
 	else if (option == "Green Party") {
-		color = 'rgba(255, 0, 0, 0.5)';
+		color = 'rgba(189, 216, 110, 0.5)';
 	}
 	else if (option == "Renua") {
-		color = 'rgba(255, 0, 0, 0.5)';
+		color = 'rgba(98, 202, 231, 0.5)';
 	}
 
 	// add transparent banner
